4ed9950f33e60d754f6084494600e6294d8f6e3a
metalmount <sokunbitaiwo82@gmail.com>
2017-04-10 11:05:14 +0100

    Update  Real Life Model for OOP using Watch example

--- watch.js
+++ watch.js
@@ -4,29 +4,25 @@
 function Watch(make){
   this.make = make;
   var date = new Date();
-  this.hour = date.getHours();
+  this.hour = date.getHours() % 12 || 12;
   this.minutes =date.getMinutes();
   this.seconds = date.getSeconds();
   
 }
 
-Watch.prototype.diplay = function(){
-    
-  if( this.hour === 0){
-      this.hour = 12;
-    }
-  else if(this.hour>12){
-    this.hour = this.hour - 12;
-    }
-  else{
-    this.hour = this.hour;
-    }
+Watch.prototype.display = function(){
+
     alert(this.make+" time is "+ this.hour + ' : '+ this.minutes );
   };
 
+
+var watch1 = new Watch("Rolex");
+watch1.display();
+
+
 /*
-var time = new Watch("Rolex");
-time.diplay();*/
+ create a subclass DigitalWatch that inherit for Watch
+*/
 
 function DigitalWatch(make, light, alarm,alarm_info,alarm_time){
   Watch.call(this,make);
@@ -48,17 +44,15 @@ DigitalWatch.prototype.constructor = DigitalWatch;
 
 DigitalWatch.prototype.display = function(){
   
-  if( this.hour === 0){
-      this.hour = 0;
-    }
-  else if(this.hour>12){
-    this.hour = this.hour;
-    }
-  else{
-    this.hour = this.hour;
-    }
+   var ampm = this.hour >= 12 ? 'pm' : 'am';
+  this.hour = this.hour % 12;
+  this.hour = this.hour ? this.hour : 12; // the hour '0' should be '12'
+  this.minutes = this.minutes < 10 ? '0'+this.minutes : this.minutes;
+  this.seconds = this.seconds < 10 ? '0'+this.seconds : this.seconds;
+ 
+ 
     
-  alert(this.make+" time is "+ this.hour + ' : '+ this.seconds +" : " +this.minutes );
+  alert(this.make+" time is "+ this.hour  +" : " +this.minutes+ ' : '+ this.seconds +" "+ ampm );
     
    
 };
@@ -66,31 +60,64 @@ DigitalWatch.prototype.display = function(){
 //Function to trigger the alarm
 DigitalWatch.prototype.triger_alarm = function(){
   if(this.alarm == "on"){
-    alert("Alarm is ON");
+    alert("DigitalWatch Alarm is ON");
     if(this.time[0] ===this.hour && this.time[1] === this.minutes && this.time[2]==this.seconds){
       alert("Reminder "+ this.alarm_info )
     }
   }else{
-    alert("Alarm OFF")
+    alert("DigitalWatch Alarm OFF")
   }
 };
 
 // Function to trigger the 
 DigitalWatch.prototype.trigger_light = function(){
     if(this.light === "on"){
-      alert("Light ON ");
+      alert(" DigitalWatch Light ON ");
     }
     else{
-      alert("Light OFF");
+      alert("DigitalWatch Light OFF");
     }
   };
 
-
-
+// function to set the time
+DigitalWatch.prototype.set_time = function(hour,minutes,seconds){
+  this.hour = hour;
+  this.minutes = minutes;
+  this.seconds =seconds;
+  
+}
+// create an object for the DigitalWatch
 var digitalwatch1 = new DigitalWatch("Swatch","on","off",[7,42,55]);
-
+digitalwatch1.set_time(13,45,15);
 digitalwatch1.display();
 digitalwatch1.triger_alarm();
 
 digitalwatch1.trigger_light();
+
+
+
+/*
+ create a subclass AnalogWatch that inherit for Watch
+*/
+function AnalogWatch(make){
+  Watch.call(this,make);
+  
+}
+
+AnalogWatch.prototype = Object.create(Watch.prototype);
+AnalogWatch.prototype.constructor = AnalogWatch;
+
+// create a new function for the AnalogWatch
+AnalogWatch.prototype.wind = function(hour,minutes){
+  this.hour = hour;
+  this.minutes = minutes;
+  
+};
+
+var analogwatch1= new AnalogWatch("Survivor");
+
+analogwatch1.wind(12,45); // use wind function to update the time
+analogwatch1.display();    // display the updated time
+
+// I need to improve on how to display the analog hands
 
